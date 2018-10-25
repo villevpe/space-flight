4bf46e88baa623be370905665b2cafb937e58ae7
villevpe <villevpe@users.noreply.github.com>
2018-10-25 21:09:39 +0300

    Update server, index and manifest

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
+const networkInterfaces = require('os').networkInterfaces
+const http = require('http')
 const nodeStatic = require('node-static')
-const fileServer = new nodeStatic.Server('./dist', { gzip: true, cache: 3600, serverInfo: 'server' })
 
-require('http')
-  .createServer((request, response) => {
-    request
-      .addListener('end', () => fileServer.serve(request, response))
-      .resume()
+const getLocalAddress = () => Object
+  .values(networkInterfaces())
+  .map(info => info.find(({ family, internal }) => family === 'IPv4' && !internal))
+  .filter(Boolean)[0].address
+
+const startServer = () => {
+  const port = parseInt(process.env.PORT, 10) || 8080
+  const server = new nodeStatic.Server('./dist', {
+    gzip: true,
+    cache: 3600,
+    serverInfo: 'server'
   })
-  .listen(process.env.PORT || 8080)
-console.log('Server started')
+
+  http
+    .createServer((request, response) => {
+      request
+        .addListener('end', () => server.serve(request, response))
+        .resume()
+    })
+    .listen(port)
+
+  console.log(`Server started @ ${getLocalAddress()}:${port}`)
+}
+
+startServer()
